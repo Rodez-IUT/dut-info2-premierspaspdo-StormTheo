6e0affd701d3b5d16cec60b769e43edc55c1f457
Théo Bouchouieff <theo.bouchouieff@iut-rodez.fr>
2019-11-13 10:32:20 +0100

    création script + finition all_users.php

--- all_users.php
+++ all_users.php
@@ -1,32 +1,53 @@
+<!DOCTYPE html>
+<HTML lang="fr">
+	<HEAD>
+		<meta charset="utf-8" />
+		<TITLE>All_users.php</TITLE>
+		<META NAME="All users">
+		
+		<!-- Bootstrap CSS -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+	</HEAD>
+	
+	<body>
+	
+		<?php
 
+		$host = 'localhost';
+		$port = '8080';
+		$db   = 'my-activities';
+		$user = 'root';
+		$pass = 'root';
+		$charset = 'utf8mb4';
+		$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+		$options = [
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+		
+		try {
+			$pdo = new PDO($dsn, $user, $pass, $options);
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+			throw new PDOException($e->getMessage(), (int)$e->getCode());
+		}
 
-$host = 'localhost';
-$db   = ‘my-activities’;
-$user = 'root';
-$pass = 'root';
-$charset = ‘utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+		$stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id = status.id ORDER BY username');
+		var_dump($stmt);
+		echo '<table>';
+		while ($row = $stmt->fetch()) {
+			var_dump($row);
+			echo '<tr>';
+			echo '<td>'.$row['user_id'].'</td>';
+			echo '<td>'.$row['username'].'</td>';
+			echo '<td>'.$row['email'].'</td>';
+			echo '<td>'.$row['name'].'</td>';
+			echo '</tr>';
+		}
+		echo '</table>';
+		?>
 
-$stmt = $pdo->query('SELECT email FROM users');
-while ($row = $stmt->fetch())
-{
-    echo $row['email'] . "\n";
-}
-
-
-try {
-    $pdo->beginTransaction();
-    $stmt = $pdo->prepare("INSERT INTO users (name) VALUES (?)");
-}catch (PDOException $e){
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+	</body>
+</HTML>
