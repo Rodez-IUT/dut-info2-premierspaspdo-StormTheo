ca5c32d792ff5972808192d95b33b292f63f16c7
THEO BOUCHOUIEFF <theo.bouchouieff@iut.local>
2019-11-20 09:45:48 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -1,53 +1,53 @@
-<!DOCTYPE html>
-<HTML lang="fr">
-	<HEAD>
-		<meta charset="utf-8" />
-		<TITLE>All_users.php</TITLE>
-		<META NAME="All users">
-		
-		<!-- Bootstrap CSS -->
-		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-		<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-	</HEAD>
-	
-	<body>
-	
-		<?php
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <title>all_users</title>
+</head>
+<body>
+    <h1>All Users</h1>
+    <table>
+    <?php
+        $host = 'localhost';
+        $port = '3306';
+        $db   = 'my_activities';
+        $user = 'root';
+        $pass = 'root';
+        $charset = 'utf8mb4';  
+        
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
 
-		$host = 'localhost';
-		$port = '8080';
-		$db   = 'my-activities';
-		$user = 'root';
-		$pass = 'root';
-		$charset = 'utf8mb4';
-		$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-		$options = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
-		
-		try {
-			$pdo = new PDO($dsn, $user, $pass, $options);
-		} catch (PDOException $e) {
-			echo $e->getMessage();
-			throw new PDOException($e->getMessage(), (int)$e->getCode());
-		}
+            echo '<tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                  </tr>';
+            
+            
+            $stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id = status.id ORDER BY username');
+            while ($row = $stmt->fetch()) {
+                echo '<tr>';
+                echo '<td>'.$row['id'].'</td>';
+                echo '<td>'.$row['username'].'</td>';
+                echo '<td>'.$row['email'].'</td>';
+                echo '<td>'.$row['name'].'</td>';
+                echo '</tr>';
+            }
 
-		$stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id = status.id ORDER BY username');
-		var_dump($stmt);
-		echo '<table>';
-		while ($row = $stmt->fetch()) {
-			var_dump($row);
-			echo '<tr>';
-			echo '<td>'.$row['user_id'].'</td>';
-			echo '<td>'.$row['username'].'</td>';
-			echo '<td>'.$row['email'].'</td>';
-			echo '<td>'.$row['name'].'</td>';
-			echo '</tr>';
-		}
-		echo '</table>';
-		?>
-
-	</body>
-</HTML>
+    ?>
+    </table>
+</body>
+</html>
