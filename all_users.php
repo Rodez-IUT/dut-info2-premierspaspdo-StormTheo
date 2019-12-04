f62315906f698a176d95b118b619428ef608fc18
THEO BOUCHOUIEFF <theo.bouchouieff@iut.local>
2019-12-04 11:46:56 +0100

    fix #5.4 Explication suite à erreur provoquée

--- all_users.php
+++ all_users.php
@@ -21,61 +21,89 @@
 				PDO::ATTR_ERRMODE				=>PDO::ERRMODE_EXCEPTION,
 				PDO::ATTR_DEFAULT_FETCH_MODE	=>PDO::FETCH_ASSOC,
 				PDO::ATTR_EMULATE_PREPARES		=>false,];
-			try{
-				$pdo=new PDO($dsn,$user,$pass,$options);
-			}catch(PDOException$e){
-				throw new PDOException($e->getMessage(),(int)$e->getCode());
+			try {
+				$pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+			echo $e->getMessage();
+			throw new PDOException($e->getMessage(), (int)$e->getCode());
 			}
-		?>
 
-		<form action="all_users.php" method="post">
-			<input id="lettre" name="lettre" type="texte"></input>
-			<select id="status_id" name="status_id">
-			<option value="2">Active account</option>
-			<option value="1">Waiting for account validation</option>
-			<option value="3">Waiting for account deletion</option></select>
-			<input type="submit" name="validation" value="rechercher"/>
-		</form>
+function get($name) {
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+// set action to trigger
+$action = get('action') ?: 'searchUsers';
+$searchStmt = null;
+// trigger the appropriate action
+$action($pdo);
+function searchUsers($pdo){
+    global $searchStmt;
+    $status_id = (int)get('status_id') ?: 2;
+    $start_letter = htmlspecialchars(get('start_letter') . '%') ?: '%';
+    $sql = "select users.id as user_id, username, email, s.name as status, s.id as status_id 
+            from users join status s on users.status_id = s.id 
+            where username like :start_letter and status_id = :status_id order by username";
+    $searchStmt = $pdo->prepare($sql);
+    $searchStmt->execute(['start_letter' => $start_letter, 'status_id' => $status_id]);
+}
+function askDeletion($pdo){
+    $user_id = (int)get("user_id");
+    // insert log
+    $sql2 = "insert into action_log (action_date, action_name, user_id) 
+              values (CURRENT_TIME(),'askDeletion',?)";
+    $stmt2 = $pdo->prepare($sql2);
+    $stmt2->execute([$user_id]);
+    // update user
+    //throw new Exception("erreur !");
+    $sql1 = "update users set status_id = 3 where id = ?";
+    $stmt1 = $pdo->prepare($sql1);
+    $stmt1->execute([$user_id]);
+    // update user list
+    searchUsers($pdo);
+}
+?>
 
-		<?php
 
-			if(isset($_POST['status_id'])){
-				$status_id = $_POST['status_id'];
-				$lettre = $_POST['lettre'];
+<h1>All Users</h1>
 
-				$stmt = $pdo->prepare("SELECT users.id,username,email,name 
-								 FROM users 
-								 JOIN status 
-								 ON users.status_id = status.id 
-								 AND status_id = :status_id
-								 AND username LIKE :username
-								 ORDER BY username");
+<form action="all_users.php" method="get">
+    <input hidden name="action" value="searchUsers">
+    Start with letter:
+    <input name="start_letter" type="text" value="<?php echo $_GET["start_letter"] ?>">
+    and status is:
+    <select name="status_id">
+        <option value="1" <?php if (get('status_id') == 1) echo 'selected' ?>>Waiting for account validation</option>
+        <option value="2" <?php if (get('status_id') == 2) echo 'selected' ?>>Active account</option>
+        <option value="3" <?php if (get('status_id') == 3) echo 'selected' ?>>Waiting for account deletion</option>
+    </select>
+    <input type="submit" value="OK">
+</form>
 
-				$stmt->bindValue(':status_id', $status_id, PDO::PARAM_INT);
-				$stmt->bindValue(':username', $lettre.'%', PDO::PARAM_STR);
-				$stmt->execute();
 
-			}else{
-				$stmt = $pdo->query("SELECT users.id,username,email,name 
-								 FROM users 
-								 JOIN status 
-								 ON users.status_id = status.id 
-								 ORDER BY username");
-			}
-			echo "<table border=\"1px\">";
-			echo "<tr><td>id</td><td>username</td><td>email</td><td>name</td></tr>";
-			while($row = $stmt->fetch()){
-				echo "<tr>";
-				echo "<td>".$row['id']."</td>";
-				echo "<td>".$row['username']."</td>";
-				echo "<td>".$row['email']."</td>";
-				echo "<td>".$row['name']."</td>";
-				if($row['name']!="Waiting for account deletion"){
-					echo "<td><a href=\"all_users.php?status=3&amp;id=".$row['id']."&amp;action=AskDeletion\">Ask deletion</a></td>";
-				}
-				echo "</tr>";
-			}
-			echo "</table>";
-		?>
-	</body>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+        <th></th>
+    </tr>
+    <?php while ($row = $searchStmt->fetch()) { ?>
+        <tr>
+            <td><?php echo $row['user_id'] ?></td>
+            <td><?php echo $row['username'] ?></td>
+            <td><?php echo $row['email'] ?></td>
+            <td><?php echo $row['status'] ?></td>
+            <td>
+                <?php if ($row['status_id'] != 3) { ?>
+                    <a href="all_users.php?status_id=3&user_id=<?php echo $row['user_id'] ?>&action=askDeletion">Ask
+                        deletion</a>
+                <?php } ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
+
+</body>
 </html>
